c3455677e6bd864c80247e850d81e976bc321dc0
赵子晗 <15802920568@163.com>
2021-03-24 18:30:52 +0800

    完成分类页面渲染及点击跳转

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,17 +1,17 @@
 <template>
-	<view>
-		列表
-	</view>
+  <view>
+   goods_list
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
 </script>
 
 <style lang="scss">
